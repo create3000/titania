a3f50354d942303edecadbf35d90735a6f78bac4
Holger Seelig <holger.seelig@yahoo.de>
2013-07-05 16:23:29 +0200

    Titania
    Updated Billboard for correct rotation.

--- libtitania-x3d/Titania/X3D/Components/Navigation/Billboard.cpp
+++ libtitania-x3d/Titania/X3D/Components/Navigation/Billboard.cpp
@@ -100,7 +100,7 @@ Billboard::transform (TraverseType type)
 
 	Matrix4f inverseModelViewMatrix = ~modelViewMatrix;
 
-	Vector3f billboardToViewer = -modelViewMatrix .translation () * inverseModelViewMatrix;
+	Vector3f billboardToViewer = inverseModelViewMatrix .translation ();
 
 	if (axisOfRotation () == Vector3f ())
 	{
@@ -125,26 +125,12 @@ Billboard::transform (TraverseType type)
 	}
 	else
 	{
-		Vector3f y = cross (billboardToViewer, axisOfRotation () .getValue ());                    // normal of plane 1
-		Vector3f n = cross (y, zAxis);                                                             // normal of plane 2
-		Vector3f z = dot (normalize (billboardToViewer), zAxis) < 0 ? cross (y, n) : cross (n, y); // intersection direction of plane 1 and 2
-		Vector3f x = cross (y, z);
+		Vector3f vector = cross (billboardToViewer, axisOfRotation () .getValue ()); // normal of plane
+		float    angle  = std::acos (dot (zAxis, normalize (axisOfRotation () .getValue ())));
 
-		//
+		Vector3f z = Rotation4f (vector, -angle) * axisOfRotation () .getValue ();
 
-		x .normalize ();
-		y .normalize ();
-		z .normalize ();
-
-		Matrix4f rotation (x [0], x [1], x [2], 0,
-		                   y [0], y [1], y [2], 0,
-		                   z [0], z [1], z [2], 0,
-		                   0,     0,     0,     1);
-
-		glMultMatrixf (rotation .data ());
-
-		// Correct rotation
-		glMultMatrixf (Matrix4f (Rotation4f (axisOfRotation () .getValue (), xAxis)) .data ());
+		glMultMatrixf (Matrix4f (Rotation4f (zAxis, z)) .data ());
 	}
 }
 
