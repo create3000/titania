9c3c8f944e7ea3f54fb6f4c2d75e708837521959
Holger Seelig <holger.seelig@yahoo.de>
2013-11-29 15:08:25 +0100

    Titania
    Work on MaterialEditor +++.

---
+++ Titania/Titania/MaterialEditor/MaterialEditor.cpp
@@ -0,0 +1,472 @@
+/* -*- Mode: C++; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraﬂe 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * THIS IS UNPUBLISHED SOURCE CODE OF create3000.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 1999, 2012 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the Titania Project.
+ *
+ * Titania is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * Titania is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with Titania.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+#include "MaterialEditor.h"
+
+#include "../Browser/BrowserWindow.h"
+#include "../Configuration/config.h"
+
+namespace titania {
+namespace puck {
+
+MaterialEditor::MaterialEditor (BrowserWindow* const browserWindow, X3D::MFNode nodes) :
+	          X3DBaseInterface (browserWindow, browserWindow -> getBrowser ()),
+	X3DMaterialEditorInterface (get_ui ("MaterialEditor.ui"), gconf_dir ()),
+	            browserSurface (X3D::createBrowser ()),
+	               appearances (),
+	                  material (new X3D::Material (browserWindow -> getBrowser () -> getExecutionContext ())),
+	          twoSidedMaterial (new X3D::TwoSidedMaterial (browserWindow -> getBrowser () -> getExecutionContext ())),
+	               initialized (false)
+{
+	material -> setup ();
+	twoSidedMaterial -> setup ();
+
+	getWindow () .set_transient_for (getBrowserWindow () -> getWindow ());
+
+	initDialog (getDiffuseDialog (),  &MaterialEditor::on_diffuseColor);
+	initDialog (getSpecularDialog (), &MaterialEditor::on_specularColor);
+	initDialog (getEmissiveDialog (), &MaterialEditor::on_emissiveColor);
+
+	initDialog (getBackDiffuseDialog (),  &MaterialEditor::on_backDiffuseColor);
+	initDialog (getBackSpecularDialog (), &MaterialEditor::on_backSpecularColor);
+	initDialog (getBackEmissiveDialog (), &MaterialEditor::on_backEmissiveColor);
+
+	// Find Appearances
+
+	X3D::traverse (nodes, [&] (X3D::SFNode & node)
+	               {
+	                  auto appearance = dynamic_cast <X3D::Appearance*> (node .getValue ());
+
+	                  if (appearance)
+								appearances .emplace_back (appearance);
+
+	                  return true;
+						});
+
+	// Find last Material and TwoSidedMaterial node
+
+	size_t materialIndex = 0;
+
+	for (const auto & appearance : basic::reverse_adapter (appearances))
+	{
+		X3D::X3DSFNode <X3D::Material> m (appearance -> material ());
+
+		if (m)
+		{
+			material -> ambientIntensity () = m -> ambientIntensity ();
+			material -> diffuseColor ()     = m -> diffuseColor ();
+			material -> specularColor ()    = m -> specularColor ();
+			material -> emissiveColor ()    = m -> emissiveColor ();
+			material -> shininess ()        = m -> shininess ();
+			material -> transparency ()     = m -> transparency ();
+			break;
+		}
+
+		++ materialIndex;
+	}
+
+	size_t twoSidedMaterialIndex = 0;
+
+	for (const auto & appearance : basic::reverse_adapter (appearances))
+	{
+		X3D::X3DSFNode <X3D::TwoSidedMaterial> m (appearance -> material ());
+
+		if (m)
+		{
+			twoSidedMaterial -> ambientIntensity () = m -> ambientIntensity ();
+			twoSidedMaterial -> diffuseColor ()     = m -> diffuseColor ();
+			twoSidedMaterial -> specularColor ()    = m -> specularColor ();
+			twoSidedMaterial -> emissiveColor ()    = m -> emissiveColor ();
+			twoSidedMaterial -> shininess ()        = m -> shininess ();
+			twoSidedMaterial -> transparency ()     = m -> transparency ();
+
+			twoSidedMaterial -> backAmbientIntensity () = m -> backAmbientIntensity ();
+			twoSidedMaterial -> backDiffuseColor ()     = m -> backDiffuseColor ();
+			twoSidedMaterial -> backSpecularColor ()    = m -> backSpecularColor ();
+			twoSidedMaterial -> backEmissiveColor ()    = m -> backEmissiveColor ();
+			twoSidedMaterial -> backShininess ()        = m -> backShininess ();
+			twoSidedMaterial -> backTransparency ()     = m -> backTransparency ();
+			break;
+		}
+
+		++ twoSidedMaterialIndex;
+	}
+
+	getFrontAndBackButton () .set_active (twoSidedMaterialIndex < materialIndex);
+}
+
+void
+MaterialEditor::initialize ()
+{
+	X3DMaterialEditorInterface::initialize ();
+
+	browserSurface -> initialized () .addInterest (this, &MaterialEditor::set_splashScreen);
+
+	// Insert Surface, this will initialize the Browser.
+	getPreviewBox () .pack_start (*browserSurface, true, true, 0);
+
+	// Show Surface and start the X3D Main Loop.
+	browserSurface -> show ();
+}
+
+void
+MaterialEditor::set_splashScreen ()
+{
+	browserSurface -> initialized () .removeInterest (this, &MaterialEditor::set_splashScreen);
+	browserSurface -> initialized () .addInterest (this, &MaterialEditor::set_initialized);
+
+	try
+	{
+		browserSurface -> loadURL ({ find_data_file ("ui/Material.x3dv") });
+	}
+	catch (const X3D::X3DError &)
+	{ }
+}
+
+void
+MaterialEditor::set_initialized ()
+{
+	on_frontAndBackButton_toggled ();
+	
+	initialized = true;
+}
+
+void
+MaterialEditor::on_frontAndBackButton_toggled ()
+{
+	// Copy front material
+
+	if (initialized)
+	{
+		if (getFrontAndBackButton () .get_active ())
+		{
+			twoSidedMaterial -> ambientIntensity () = material -> ambientIntensity ();
+			twoSidedMaterial -> diffuseColor ()     = material -> diffuseColor ();
+			twoSidedMaterial -> specularColor ()    = material -> specularColor ();
+			twoSidedMaterial -> emissiveColor ()    = material -> emissiveColor ();
+			twoSidedMaterial -> shininess ()        = material -> shininess ();
+			twoSidedMaterial -> transparency ()     = material -> transparency ();
+		}
+		else
+		{
+			material -> ambientIntensity () = twoSidedMaterial -> ambientIntensity ();
+			material -> diffuseColor ()     = twoSidedMaterial -> diffuseColor ();
+			material -> specularColor ()    = twoSidedMaterial -> specularColor ();
+			material -> emissiveColor ()    = twoSidedMaterial -> emissiveColor ();
+			material -> shininess ()        = twoSidedMaterial -> shininess ();
+			material -> transparency ()     = twoSidedMaterial -> transparency ();
+		}
+	}
+
+	// Update material
+
+	try
+	{
+		X3D::X3DSFNode <X3D::Appearance> appearance (browserSurface -> getExecutionContext () -> getNamedNode ("Appearance"));
+
+		if (appearance)
+		{
+			if (getFrontAndBackButton () .get_active ())
+				appearance -> material () = twoSidedMaterial;
+
+			else
+				appearance -> material () = material;
+		}
+	}
+	catch (const X3D::X3DError &)
+	{ }
+
+	// Update color buttons
+
+	getDiffuseArea ()  .queue_draw ();
+	getSpecularArea () .queue_draw ();
+	getEmissiveArea () .queue_draw ();
+
+	getBackDiffuseArea ()  .queue_draw ();
+	getBackSpecularArea () .queue_draw ();
+	getBackEmissiveArea () .queue_draw ();
+
+	// Back expander
+
+	getBackExpander () .set_sensitive (getFrontAndBackButton () .get_active ());
+	
+	// Update browser immediately
+
+	getBrowser () -> update ();
+}
+
+// Diffuse color
+
+bool
+MaterialEditor::on_diffuse_draw (const Cairo::RefPtr <Cairo::Context> & context)
+{
+	return on_color_draw (context, twoSidedMaterial -> diffuseColor (), material -> diffuseColor (), getDiffuseArea ());
+}
+
+bool
+MaterialEditor::on_diffuse_released (GdkEventButton* event)
+{
+	return on_color_released (event, getDiffuseDialog (), twoSidedMaterial -> diffuseColor (), material -> diffuseColor ());
+}
+
+void
+MaterialEditor::on_diffuseColor ()
+{
+	on_color (getDiffuseDialog (), twoSidedMaterial -> diffuseColor (), material -> diffuseColor (), getDiffuseArea ());
+}
+
+// Specular color
+
+bool
+MaterialEditor::on_specular_draw (const Cairo::RefPtr <Cairo::Context> & context)
+{
+	return on_color_draw (context, twoSidedMaterial -> specularColor (), material -> specularColor (), getSpecularArea ());
+}
+
+bool
+MaterialEditor::on_specular_released (GdkEventButton* event)
+{
+	return on_color_released (event, getSpecularDialog (), twoSidedMaterial -> specularColor (), material -> specularColor ());
+}
+
+void
+MaterialEditor::on_specularColor ()
+{
+	on_color (getSpecularDialog (), twoSidedMaterial -> specularColor (), material -> specularColor (), getSpecularArea ());
+}
+
+// Emissive color
+
+bool
+MaterialEditor::on_emissive_draw (const Cairo::RefPtr <Cairo::Context> & context)
+{
+	return on_color_draw (context, twoSidedMaterial -> emissiveColor (), material -> emissiveColor (), getEmissiveArea ());
+}
+
+bool
+MaterialEditor::on_emissive_released (GdkEventButton* event)
+{
+	return on_color_released (event, getEmissiveDialog (), twoSidedMaterial -> emissiveColor (), material -> emissiveColor ());
+}
+
+void
+MaterialEditor::on_emissiveColor ()
+{
+	on_color (getEmissiveDialog (), twoSidedMaterial -> emissiveColor (), material -> emissiveColor (), getEmissiveArea ());
+}
+
+// Diffuse color
+
+bool
+MaterialEditor::on_backDiffuse_draw (const Cairo::RefPtr <Cairo::Context> & context)
+{
+	return on_color_draw (context, twoSidedMaterial -> backDiffuseColor (), twoSidedMaterial -> backDiffuseColor (), getBackDiffuseArea ());
+}
+
+bool
+MaterialEditor::on_backDiffuse_released (GdkEventButton* event)
+{
+	return on_color_released (event, getBackDiffuseDialog (), twoSidedMaterial -> backDiffuseColor (), twoSidedMaterial -> backDiffuseColor ());
+}
+
+void
+MaterialEditor::on_backDiffuseColor ()
+{
+	on_color (getBackDiffuseDialog (), twoSidedMaterial -> backDiffuseColor (), twoSidedMaterial -> backDiffuseColor (), getBackDiffuseArea ());
+}
+
+// Specular color
+
+bool
+MaterialEditor::on_backSpecular_draw (const Cairo::RefPtr <Cairo::Context> & context)
+{
+	return on_color_draw (context, twoSidedMaterial -> backSpecularColor (), twoSidedMaterial -> backSpecularColor (), getBackSpecularArea ());
+}
+
+bool
+MaterialEditor::on_backSpecular_released (GdkEventButton* event)
+{
+	return on_color_released (event, getBackSpecularDialog (), twoSidedMaterial -> backSpecularColor (), twoSidedMaterial -> backSpecularColor ());
+}
+
+void
+MaterialEditor::on_backSpecularColor ()
+{
+	on_color (getBackSpecularDialog (), twoSidedMaterial -> backSpecularColor (), twoSidedMaterial -> backSpecularColor (), getBackSpecularArea ());
+}
+
+// Emissive color
+
+bool
+MaterialEditor::on_backEmissive_draw (const Cairo::RefPtr <Cairo::Context> & context)
+{
+	return on_color_draw (context, twoSidedMaterial -> backEmissiveColor (), twoSidedMaterial -> backEmissiveColor (), getBackEmissiveArea ());
+}
+
+bool
+MaterialEditor::on_backEmissive_released (GdkEventButton* event)
+{
+	return on_color_released (event, getBackEmissiveDialog (), twoSidedMaterial -> backEmissiveColor (), twoSidedMaterial -> backEmissiveColor ());
+}
+
+void
+MaterialEditor::on_backEmissiveColor ()
+{
+	on_color (getBackEmissiveDialog (), twoSidedMaterial -> backEmissiveColor (), twoSidedMaterial -> backEmissiveColor (), getBackEmissiveArea ());
+}
+
+// Operations for all colors
+
+void
+MaterialEditor::initDialog (Gtk::ColorSelectionDialog & dialog, void (MaterialEditor::* callback)())
+{
+	dialog .get_color_selection () -> set_has_palette (true);
+	dialog .get_color_selection () -> signal_color_changed () .connect (sigc::mem_fun (*this, callback));
+	dialog .property_ok_button () .get_value () -> hide ();
+	dialog .property_cancel_button () .get_value () -> hide ();
+}
+
+bool
+MaterialEditor::on_color_draw (const Cairo::RefPtr <Cairo::Context> & context, const X3D::Color3f & twoSidedColor, const X3D::Color3f & color, Gtk::DrawingArea & drawingArea)
+{
+	if (getFrontAndBackButton () .get_active ())
+		context -> set_source_rgb (twoSidedColor .r (), twoSidedColor .g (), twoSidedColor .b ());
+
+	else
+		context -> set_source_rgb (color .r (), color .g (), color .b ());
+
+	context -> rectangle (0, 0, drawingArea .get_width (), drawingArea .get_height ());
+	context -> fill ();
+
+	return true;
+}
+
+bool
+MaterialEditor::on_color_released (GdkEventButton* event, Gtk::ColorSelectionDialog & dialog, const X3D::Color3f & twoSidedColor, const X3D::Color3f & color)
+{
+	if (event -> button == 1)
+	{
+		if (getFrontAndBackButton () .get_active ())
+		{
+			dialog .get_color_selection () -> set_current_color  (toColor (twoSidedColor));
+			dialog .get_color_selection () -> set_previous_color (toColor (twoSidedColor));
+		}
+		else
+		{
+			dialog .get_color_selection () -> set_current_color  (toColor (color));
+			dialog .get_color_selection () -> set_previous_color (toColor (color));
+		}
+
+		dialog .present ();
+	}
+
+	return true;
+}
+
+void
+MaterialEditor::on_color (Gtk::ColorSelectionDialog & dialog, X3D::SFColor & twoSidedColor, X3D::SFColor & color, Gtk::DrawingArea & drawingArea)
+{
+	auto rgba = dialog .get_color_selection () -> get_current_rgba ();
+
+	X3D::Color3f color3 (rgba .get_red (), rgba .get_green (), rgba .get_blue ());
+
+	// Update material
+
+	if (getFrontAndBackButton () .get_active ())
+	{
+		twoSidedColor = color3;
+
+		for (const auto & appearance : appearances)
+			appearance -> material () = twoSidedMaterial;
+	}
+	else
+	{
+		color = color3;
+
+		for (const auto & appearance : appearances)
+			appearance -> material () = material;
+	}
+
+	// Update materials
+
+	getBrowser () -> update ();
+
+	drawingArea .queue_draw ();
+	
+	browserSurface -> addEvent ();
+}
+
+// Helper functions
+
+Gdk::Color
+MaterialEditor::toColor (const X3D::Color3f & color)
+{
+	Gdk::Color rgb;
+
+	rgb .set_rgb_p (color .r (), color .g (), color .b ());
+	return rgb;
+}
+
+Gdk::Color
+MaterialEditor::toColor (float value)
+{
+	Gdk::Color rgb;
+
+	rgb .set_rgb_p (value, value, value);
+	return rgb;
+}
+
+MaterialEditor::~MaterialEditor ()
+{
+	__LOG__ << std::endl;
+}
+
+} // puck
+} // titania
