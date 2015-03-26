8feac7fea8a3fd73ff24d9fba06882468c702e51
Holger Seelig <holger.seelig@yahoo.de>
2015-03-26 12:32:42 +0100

    Titania
    Fixed issue with undo of bbox fields in Precision Placement Panel.

--- Titania/Titania/Editors/PrecisionPlacementPanel/PrecisionPlacementPanel.cpp
+++ Titania/Titania/Editors/PrecisionPlacementPanel/PrecisionPlacementPanel.cpp
@@ -147,23 +147,27 @@ PrecisionPlacementPanel::on_fill_bounding_box_fields_clicked ()
 
 	X3D::X3DPtr <X3D::X3DGroupingNode> group (boundedObject);
 
+	const X3D::Vector3f bboxSize1   = boundedObject -> bboxSize ();
+	const X3D::Vector3f bboxCenter1 = boundedObject -> bboxCenter ();
+
+	// Reset to get calculated bbox.
 	boundedObject -> bboxSize ()   = X3D::Vector3f (-1, -1, -1);
 	boundedObject -> bboxCenter () = X3D::Vector3f ();
 
-	const auto bbox       = group ? group -> X3DGroupingNode::getBBox () : boundedObject -> getBBox ();
-	const auto bboxSize   = bbox .size ();
-	const auto bboxCenter = bbox .center ();
-	const auto undoStep   = std::make_shared <UndoStep> (_ ("Fill Bounding Box Fields"));
+	const auto bbox2       = group ? group -> X3DGroupingNode::getBBox () : boundedObject -> getBBox ();
+	const auto bboxSize2   = bbox2 .size ();
+	const auto bboxCenter2 = bbox2 .center ();
+	const auto undoStep    = std::make_shared <UndoStep> (_ ("Fill Bounding Box Fields"));
 
 	undoStep -> addObjects (boundedObject);
-	undoStep -> addUndoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxSize ()), boundedObject -> bboxSize ());
-	undoStep -> addRedoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxSize ()), bboxSize);
-	boundedObject -> bboxSize () = bboxSize;
+	undoStep -> addUndoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxSize ()), bboxSize1);
+	undoStep -> addRedoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxSize ()), bboxSize2);
+	boundedObject -> bboxSize () = bboxSize2;
 
 	undoStep -> addObjects (boundedObject);
-	undoStep -> addUndoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxCenter ()), boundedObject -> bboxCenter ());
-	undoStep -> addRedoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxCenter ()), bboxCenter);
-	boundedObject -> bboxCenter () = bboxCenter;
+	undoStep -> addUndoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxCenter ()), bboxCenter1);
+	undoStep -> addRedoFunction ((setValue) & X3D::SFVec3f::setValue, std::ref (boundedObject -> bboxCenter ()), bboxCenter2);
+	boundedObject -> bboxCenter () = bboxCenter2;
 
 	getBrowserWindow () -> addUndoStep (undoStep);
 }
