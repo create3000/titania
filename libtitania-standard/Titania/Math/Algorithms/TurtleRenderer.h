9d28ad57d52590b93c28d52b6de65cb598bcd176
Holger Seelig <holger.seelig@yahoo.de>
2017-04-06 22:06:39 +0200

    TurtleRenderer.

---
+++ libtitania-standard/Titania/Math/Algorithms/TurtleRenderer.h
@@ -0,0 +1,322 @@
+/* -*- Mode: C++; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraﬂe 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * THIS IS UNPUBLISHED SOURCE CODE OF create3000.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 1999, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the Titania Project.
+ *
+ * Titania is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * Titania is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with Titania.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+#ifndef __TITANIA_MATH_ALGORITHMS_TURTLE_RENDERER_H__
+#define __TITANIA_MATH_ALGORITHMS_TURTLE_RENDERER_H__
+
+#include "../Numbers/Vector3.h"
+#include "LSystem.h"
+
+#include <memory>
+#include <vector>
+
+namespace titania {
+namespace math {
+
+template <class Type>
+struct turtle_renderer_node {
+
+	using value_type    = Type;
+	using colors_type   = std::vector <int32_t>;
+	using node_ptr      = std::shared_ptr <turtle_renderer_node>;
+	using children_type = std::vector <node_ptr>;
+
+	turtle_renderer_node (const int32_t color, const vector3 <Type> & point) :
+		            colors (),
+		             color (color),
+		next_control_point (1),
+		             point (point),
+		          children ()
+	{ }
+
+	colors_type    colors;
+	int32_t        color;
+	int32_t        next_control_point;
+	vector3 <Type> point;
+	children_type  children;
+
+};
+
+template <class Type, class String>
+class turtle_renderer {
+public:
+
+	///  @name Member types
+
+	using value_type  = Type;
+	using string_type = String;
+	using node_type   = turtle_renderer_node <Type>;
+	using node_ptr    = typename node_type::node_ptr;
+
+	///  @name Construction
+
+	turtle_renderer (const Type & x_angle, const Type & y_angle, const Type & z_angle);
+
+	turtle_renderer (const Type & x_angle, const Type & y_angle, const Type & z_angle, const lsystem <String> & lsystem);
+
+	///  @name Members
+
+	const Type &
+	x_angle () const
+	{ return mx_angle; }
+
+	const Type &
+	y_angle () const
+	{ return my_angle; }
+
+	const Type &
+	z_angle () const
+	{ return mz_angle; }
+
+	bool
+	colors () const
+	{ return m_colors; }
+
+	const node_ptr &
+	tree () const
+	{ return m_tree; }
+
+	///  @name Operations
+
+	void
+	render (const lsystem <String> & lsystem);
+
+	///  @name Destruction
+
+	~turtle_renderer ();
+
+private:
+
+	///  @name Members
+
+	Type     mx_angle;
+	Type     my_angle;
+	Type     mz_angle;
+	bool     m_colors;
+	node_ptr m_tree; 
+
+};
+
+template <class Type, class String>
+turtle_renderer <Type, String>::turtle_renderer (const Type & x_angle, const Type & y_angle, const Type & z_angle) :
+	mx_angle (x_angle),
+	my_angle (y_angle),
+	mz_angle (z_angle),
+	m_colors (false),
+	  m_tree ()
+{ }
+
+template <class Type, class String>
+turtle_renderer <Type, String>::turtle_renderer (const Type & x_angle, const Type & y_angle, const Type & z_angle, const lsystem <String> & lsystem) :
+	turtle_renderer (x_angle, y_angle, z_angle)
+{
+	render (lsystem);
+}
+
+template <class Type, class String>
+void
+turtle_renderer <Type, String>::render (const lsystem <String> & lsystem)
+{
+	struct stack_value
+	{
+		stack_value (const node_ptr & node,
+		             const vector3 <Type> & point,
+		             const matrix3 <Type> & matrix,
+		             const int32_t color) :
+			  node (node),
+			 point (point),
+			matrix (matrix),
+			 color (color)
+		{ }
+	
+		const node_ptr       node;
+		const vector3 <Type> point;
+		const matrix3 <Type> matrix;
+		const int32_t        color;
+	};
+
+	bool    change    = true;
+	auto    matrix    = matrix3 <Type> ();
+	auto    point     = vector3 <Type> ();
+	int32_t color     = 0;
+	int32_t lastColor = -1;
+	auto    root      = std::make_shared <node_type> (color, point);
+	auto    node      = root;
+	auto    stack     = std::vector <stack_value> ();
+
+	std::istringstream isstream (lsystem .commands ());
+
+	while (isstream)
+	{
+		const auto c = isstream .get ();
+
+		switch (c)
+		{
+			case 'C': // Color
+			{
+				int32_t index;
+
+				if ((isstream >> index) and (index >= 0))
+				{
+					m_colors      = true;
+					change        = index not_eq lastColor;
+					color         = index;
+					node -> color = index;
+				}
+
+				break;
+			}
+			case '}': // Counterclockwise rotation about local x-axis
+			{
+				matrix *= matrix3 <Type> (rotation4 <Type> (matrix [0], mx_angle));
+				change  = true;
+				break;
+			}
+			case '{': // Clockwise rotation about local x-axis
+			{
+				matrix *= matrix3 <Type> (rotation4 <Type> (matrix [0], -mx_angle));
+				change  = true;
+				break;
+			}
+			case '>': // Counterclockwise rotation about local y-axis
+			{
+				matrix *= matrix3 <Type> (rotation4 <Type> (matrix [1], my_angle));
+				change  = true;
+				break;
+			}
+			case '<': // Clockwise rotation about local y-axis
+			{
+				matrix *= matrix3 <Type> (rotation4 <Type> (matrix [1], -my_angle));
+				change  = true;
+				break;
+			}
+			case '+': // Counterclockwise rotation about local z-axis
+			{
+				matrix *= matrix3 <Type> (rotation4 <Type> (matrix [2], mz_angle));
+				change  = true;
+				break;
+			}
+			case '-': // Clockwise rotation about local z-axis
+			{
+				matrix *= matrix3 <Type> (rotation4 <Type> (matrix [2], -mz_angle));
+				change  = true;
+				break;
+			}
+			case '[': // Push
+			{
+				stack .emplace_back (node, point, matrix, color);
+
+				change    = true;
+				lastColor = -1;
+				break;
+			}
+			case ']': // Pop
+			{
+				if (stack .empty ())
+					break;
+
+				const auto & back = stack .back ();
+
+				change    = true;
+				node      = back .node;
+				point     = back .point;
+				matrix    = back .matrix;
+				color     = back .color;
+				lastColor = -1;
+
+				stack .pop_back ();
+				break;
+			}
+			default: // Line
+			{
+				if (c < 0)
+					break;
+
+				if (lsystem .is_constant (c))
+					break;
+
+				point += matrix [1];
+
+				if (change)
+				{
+					change    = false;
+					lastColor = color;
+
+					auto child = std::make_shared <node_type> (color, point);
+	
+					node -> colors   .emplace_back (color);
+					node -> children .emplace_back (child);
+	
+					node = std::move (child);
+				}
+				else
+				{
+					node -> point = point;
+				}
+
+				break;
+			}
+		}
+	}
+
+	if (root -> children .empty ())
+		return;
+
+	m_tree = root;
+}
+
+template <class Type, class String>
+turtle_renderer <Type, String>::~turtle_renderer ()
+{ }
+
+} // math
+} // titania
+
+#endif
