c79d110f6b258c9e631541c66cb1fa217e1be9cb
Holger Seelig <holger.seelig@yahoo.de>
2017-09-26 08:48:14 +0200

    Bump version number.

--- libtitania-x3d/Titania/X3D/Browser/X3DBrowser.cpp
+++ libtitania-x3d/Titania/X3D/Browser/X3DBrowser.cpp
@@ -71,7 +71,7 @@
 namespace titania {
 namespace X3D {
 
-const std::string X3DBrowser::version = "3.0.4";
+const std::string X3DBrowser::version = "3.1.0";
 
 const ComponentType X3DBrowser::component      = ComponentType::TITANIA;
 const std::string   X3DBrowser::typeName       = "Browser";
