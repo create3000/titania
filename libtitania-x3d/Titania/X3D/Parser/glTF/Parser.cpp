4b751421d09578426e84d4a72123344c06c01f5f
Holger Seelig <holger.seelig@yahoo.de>
2018-02-14 06:05:16 +0100

    Published version 4.0.6a

--- libtitania-x3d/Titania/X3D/Parser/glTF/Parser.cpp
+++ libtitania-x3d/Titania/X3D/Parser/glTF/Parser.cpp
@@ -197,7 +197,7 @@ Parser::importProto (const std::string & name)
 	for (const auto & externproto : importedExternprotos)
 	{
 		externproto -> url () = {
-			"http://code.create3000.de/titania/alpha/shaders/glTF/Prototypes/" + name + ".x3d#" + name,
+			"http://code.create3000.de/titania/" + getBrowser () -> getVersion () + "/shaders/glTF/Prototypes/" + name + ".x3d#" + name,
 			"https://cdn.rawgit.com/create3000/titania/" + getBrowser () -> getVersion () + "/libtitania-x3d/share/titania/shaders/glTF/Prototypes/" + name + ".x3d#" + name,
 			"https://rawgit.com/create3000/titania/" + getBrowser () -> getVersion () + "/libtitania-x3d/share/titania/shaders/glTF/Prototypes/" + name + ".x3d#" + name,
 			"file:///usr/share/titania/shaders/glTF/Prototypes/" + name + ".x3d#" + name,
