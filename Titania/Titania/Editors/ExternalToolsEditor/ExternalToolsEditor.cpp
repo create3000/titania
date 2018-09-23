990a71341f04721c0127288f7fe060a3848aef9f
Holger Seelig <holger.seelig@yahoo.de>
2018-09-23 15:01:10 +0200

    Select and start editing in tree view of newly created external tool.

--- Titania/Titania/Editors/ExternalToolsEditor/ExternalToolsEditor.cpp
+++ Titania/Titania/Editors/ExternalToolsEditor/ExternalToolsEditor.cpp
@@ -148,7 +148,7 @@ ExternalToolsEditor::on_add_tool_clicked ()
 		setText (id, "#!/bin/sh\n");
 		saveTree ();
 
-		getTreeSelection () -> select (iter);
+		getTreeView () .set_cursor (getTreeStore () -> get_path (iter), *getNameColumn () .operator -> (), true);
 	}
 	catch (const Glib::Error & error)
 	{
