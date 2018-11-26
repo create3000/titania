7536963a681cb320db5bf2b3c91149d4712b6675
Holger Seelig <holger.seelig@gmail.com>
2018-11-26 11:05:20 +0100

    Updated run.pl.

--- Titania/run.pl
+++ Titania/run.pl
@@ -21,7 +21,8 @@ if (($min > 16 and $min < 20) or 0)
 	}
 }
 
-$ENV {PATH}            = "$SOLUTION_DIR/Titania/:$ENV{PATH}";
+$ENV {PATH}            = "$SOLUTION_DIR/Titania:$ENV{PATH}";
+$ENV {LD_LIBRARY_PATH} = "/usr/local/lib";
 $ENV {XDG_DATA_DIRS}   = "$SOLUTION_DIR/Titania/share:$SOLUTION_DIR/libtitania-x3d/share:$ENV{XDG_DATA_DIRS}";
 $ENV {GDK_SYNCHRONIZE} = "1";
 
