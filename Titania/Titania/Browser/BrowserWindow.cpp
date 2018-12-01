18f1c61dcd3a89b9d77dfb4f8642ca59b93737c6
Holger Seelig <holger.seelig@gmail.com>
2018-12-01 07:54:32 +0100

    Escape scene menu.

--- Titania/Titania/Browser/BrowserWindow.cpp
+++ Titania/Titania/Browser/BrowserWindow.cpp
@@ -1966,8 +1966,8 @@ BrowserWindow::on_scenes_activated (Gtk::Menu & menu)
 
 		menuItem -> set_image (*icon);
 		menuItem -> set_always_show_image (true);
-		menuItem -> set_label ((worldURL .empty () ? _ ("New Scene") : worldURL .basename ()) + (modified ? "*" : ""));
-		menuItem -> set_tooltip_text (worldURL .empty () ? _ ("New Scene") : worldURL .filename () .str ());
+		menuItem -> set_label ((worldURL .empty () ? _ ("New Scene") : worldURL .escape (" ") .basename ()) + (modified ? "*" : ""));
+		menuItem -> set_tooltip_text (worldURL .empty () ? _ ("New Scene") : worldURL .filename () .escape (" ") .str ());
 
 		if (browser == getCurrentBrowser ())
 		   menuItem -> get_style_context () -> add_class ("titania-menu-item-selected");
