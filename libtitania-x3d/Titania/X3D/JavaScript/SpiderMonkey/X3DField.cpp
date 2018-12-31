88d0077c78f1308f275b0042a99e239f2fa2cf25
Holger Seelig <holger.seelig@gmail.com>
2018-12-31 04:40:12 +0100

    Removed attr from X3DField.

--- libtitania-x3d/Titania/X3D/JavaScript/SpiderMonkey/X3DField.cpp
+++ libtitania-x3d/Titania/X3D/JavaScript/SpiderMonkey/X3DField.cpp
@@ -73,7 +73,7 @@ const JSClassOps X3DField::class_ops = {
 
 const JSClass X3DField::static_class = {
 	"X3DField",
-	JSCLASS_HAS_PRIVATE | JSCLASS_HAS_RESERVED_SLOTS (SLOTS_SIZE) | JSCLASS_FOREGROUND_FINALIZE,
+	JSCLASS_FOREGROUND_FINALIZE,
 	&class_ops
 };
 
