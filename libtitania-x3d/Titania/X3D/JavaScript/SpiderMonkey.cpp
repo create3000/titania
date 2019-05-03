411eac5470da1efd456b5644b6529d9301f221c6
Holger Seelig <holger.seelig@gmail.com>
2019-05-03 14:29:08 +0200

    Use LL for JS_NewContext.

--- libtitania-x3d/Titania/X3D/JavaScript/SpiderMonkey.cpp
+++ libtitania-x3d/Titania/X3D/JavaScript/SpiderMonkey.cpp
@@ -91,7 +91,7 @@ SpiderMonkey::initialize ()
 JSContext*
 SpiderMonkey::createContext ()
 {
-	const auto cx = JS_NewContext (2048L * 1024 * 1024);
+	const auto cx = JS_NewContext (2048LL * 1024 * 1024);
 
 	if (not cx)
 		return nullptr;
