72a35085b8a07944edd4e4a816f105fc0a638bf0
Holger Seelig <holger.seelig@gmail.com>
2019-07-06 13:31:57 +0200

    Fixed .glade files.

--- Titania/Titania/Revealer/FogCoordinateEditor/FogCoordinateEditor.cpp
+++ Titania/Titania/Revealer/FogCoordinateEditor/FogCoordinateEditor.cpp
@@ -70,9 +70,6 @@ FogCoordinateEditor::FogCoordinateEditor (X3DBrowserWindow* const browserWindow)
 {
 	addChildObjects (indexedFaceSetNodes);
 
-	getFogDepthAdjustment () -> set_step_increment (0.01);
-	getFogDepthAdjustment () -> set_page_increment (0.1);
-
 	setup ();
 }
 
