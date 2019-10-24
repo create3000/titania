fd27741d4f1643071de6197e5657a763458f4946
Holger Seelig <holger.seelig@gmail.com>
2019-10-24 11:24:15 +0200

    Use 'discard' in IsoSurfaceVolumeData.

--- libtitania-x3d/Titania/X3D/Components/VolumeRendering/IsoSurfaceVolumeData.cpp
+++ libtitania-x3d/Titania/X3D/Components/VolumeRendering/IsoSurfaceVolumeData.cpp
@@ -356,7 +356,7 @@ IsoSurfaceVolumeData::createShader () const
 
 		styleFunctions += "	else\n";
 		styleFunctions += "	{\n";
-		styleFunctions += "		textureColor = vec4 (0.0);\n";
+		styleFunctions += "		discard;\n";
 		styleFunctions += "	}\n";
 		styleFunctions += "\n";
 	}
